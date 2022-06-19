79f229e90ce93bd63cb3b57bbb9416de122c3827
Dutra-fran <sgtrobsondutra51@gmail.com>
2022-06-19 19:17:28 -0300

    Commit inicial do projeto

--- FileHandling/src/exceptions/ExistenceFileException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class ExistenceFileException extends Exception {
-    public String toString(String fileName){
-        return "Arquivo: "+ fileName + " não encontrado!";
-    }
-}
